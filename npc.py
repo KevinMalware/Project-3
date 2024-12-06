fe7223e58f16a9912ecc44b1a1c5ce11a348e5b0
KevinMalware <146421199+KevinMalware@users.noreply.github.com>
2024-12-06 00:29:37 -0500

    Update npc.py

--- npc.py
+++ npc.py
@@ -1,24 +1,77 @@
 class NPC:
+    """
+    A class to represent a Non-Player Character (NPC).
+    
+    Attributes:
+    -----------
+    name : str
+        The name of the NPC.
+    phrase : str
+        The phrase spoken by the NPC.
+    """
 
-    #we use a constructor for the npc class with the attributes of name and phrase
     def __init__(self, name, phrase):
+        """
+        Constructs all the necessary attributes for the NPC object.
+        
+        Parameters:
+        -----------
+        name : str
+            The name of the NPC.
+        phrase : str
+            The phrase spoken by the NPC.
+        """
         self.name = name
         self.phrase = phrase
 
-    #we use a get method to get the name and phrase of the NPC
     def get_name(self):
+        """
+        Returns the name of the NPC.
+
+        Returns:
+        --------
+        str : The name of the NPC.
+        """
         return self.name
 
     def get_phrase(self):
+        """
+        Returns the phrase spoken by the NPC.
+
+        Returns:
+        --------
+        str : The phrase spoken by the NPC.
+        """
         return self.phrase
 
-    #we use a setter method to update the name and phrase of the NPC
     def set_name(self, name):
+        """
+        Sets/updates the name of the NPC.
+
+        Parameters:
+        -----------
+        name : str
+            The new name for the NPC.
+        """
         self.name = name
 
     def set_phrase(self, phrase):
+        """
+        Sets/updates the phrase spoken by the NPC.
+
+        Parameters:
+        -----------
+        phrase : str
+            The new phrase for the NPC.
+        """
         self.phrase = phrase
 
-    #we have a speak method by returning a string with the name and phrase
     def speak(self):
+        """
+        Returns a formatted string of the NPC speaking.
+
+        Returns:
+        --------
+        str : A string containing the NPC's name and their phrase.
+        """
         return f'{self.name} says, "{self.phrase}"'
