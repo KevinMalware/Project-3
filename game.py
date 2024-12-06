60dcab37cec2bd6858fcc14f9c25eedb8b9d9ec0
KevinMalware <146421199+KevinMalware@users.noreply.github.com>
2024-12-06 00:26:35 -0500

    game.py

--- game.py
+++ game.py
@@ -2,17 +2,68 @@ from item import Item
 from npc import NPC
 from room import Room
 import random
-#first you need to import the item, npc, and room classes and import random
-#so you can use them later in the game.py
 
 class Game:
+    """
+    A class to represent the main game logic.
 
-    #a constructor that initalizes the game class, in this constructor
-    #there is are holders for the inventory of the player, as well as the current room of the player
-    #, a message that is going to show up to the player, the previous room of the player was in, the number of
-    #turns that player has taken, and there is also a create function that creates the world with
-    #lastly a welcome message
+    Attributes
+    ----------
+    items : list
+        A list to store player's inventory items.
+    current : Room
+        The current room the player is in.
+    msg : str
+        A message to display to the player.
+    previous_room : Room
+        The previous room the player was in.
+    turns : int
+        The number of turns the player has taken.
+
+    Methods
+    -------
+    create_world():
+        Creates the game world, including items, NPCs, and rooms.
+    set_welcome_message():
+        Sets the welcome message for the game.
+    get_message():
+        Returns the current message for the player.
+    get_current_room():
+        Returns the current room.
+    move(direction):
+        Moves the player to the next room in a given direction.
+    look():
+        Sets the message to describe the current room.
+    help():
+        Sets the message to display the available commands.
+    items():
+        Sets the message to list the items the player is holding.
+    take():
+        Adds an item in the current room to the player's inventory.
+    place(item_name):
+        Places an item from the player's inventory into the current room.
+    search_items(name):
+        Searches for an item in the player's inventory by name.
+    speak():
+        Allows the player to interact with an NPC if one is present.
+    retreat():
+        Allows the player to return to the previous room.
+    use(item_name):
+        Uses a specific item from the player's inventory.
+    gamble(choice):
+        Lets the player gamble to move forward or return to a previous room.
+    game_over():
+        Checks if the game is over (either won or lost).
+    auto_win():
+        Automatically moves the player to the winning level.
+    parse_command():
+        Reads player input and parses it into a command and an argument.
+    play():
+        Starts the game loop where the player can enter commands.
+    """
+
     def __init__(self):
+        """Initializes the game state."""
         self.items = []
         self.current = None
         self.msg = ""
@@ -21,10 +72,10 @@ class Game:
         self.create_world()
         self.set_welcome_message()
 
-    #a function is being made for the game world that has different parts of the game
     def create_world(self):
+        """Creates the items, NPCs, and rooms in the game."""
 
-        #the following below are items that are in the games, with its name, description, and weight in the parameters
+        # Create items with name, description, and weight
         self.treasure_chest = Item("treasure chest", "a chest that will give you wealth", 100)
         self.map = Item("map", "a map that lets you skip 2 levels", 5)
         self.teleporter = Item("teleporter", "a teleporter that will randomly transport you to a level (1-8)", 30)
@@ -32,14 +83,12 @@ class Game:
         self.power_shield = Item("power shield", "a shield that protects you from traps in Level 6", 15)
         self.magic_key = Item("magic key", "a key that unlocks hidden shortcuts in Levels 5 and 7", 10)
 
-        #these are the non-playable characters(npcs) that the player will be able to talk to
-        #they have the name of the npc and phrase in the parameters
+        # Create NPCs with name and phrase
         self.builderman = NPC("Builderman", "Find the map and it will decrease your journey!")
         self.telamon = NPC("Telamon", "Find the chest and you will be forever rich.")
         self.noob = NPC("Noob", "Use the power shield to avoid traps ahead!")
 
-        #these are all the levels with the description of the room and whether or not if
-        #it it has a npc or item in the room within the parameters
+        # Create rooms with description, item, and NPC
         self.level1 = Room("a dark room with glow lights everywhere", None, self.builderman)
         self.level2 = Room("a room filled with speed boost power-ups", self.speedy_cola)
         self.level3 = Room("a misty room with mysterious fog", self.magic_key, self.telamon)
@@ -49,7 +98,7 @@ class Game:
         self.level7 = Room("a cold room with icy floors")
         self.level8 = Room("a grand hall with a glowing exit door", self.treasure_chest)
 
-        #all the rooms are connected to another room with the direction being south of another
+        # Connect rooms
         self.level1.add_neighbor("south", self.level2)
         self.level2.add_neighbor("south", self.level3)
         self.level3.add_neighbor("south", self.level4)
@@ -58,27 +107,20 @@ class Game:
         self.level6.add_neighbor("south", self.level7)
         self.level7.add_neighbor("south", self.level8)
 
-        #you start the game at level 1 with this statement
+        # Start the game in Level 1
         self.current = self.level1
 
-    #this shows the game message
     def get_message(self):
+        """Returns the current game message."""
         return self.msg
 
-    #this shows the current room the player is in
     def get_current_room(self):
+        """Returns the current room the player is in."""
         return self.current
 
-    #this function moves the player to antoher room in the given direction
     def move(self, direction):
-        #this finds out if there is a certain room in a direction that the player
-        #wants to go to
+        """Moves the player in the given direction."""
         next_room = self.current.get_neighbor(direction)
-
-        #if there is no room, the player will be told that they can't move in that direction
-        #otherwise if there is a room, the player will be moved there, also in this function
-        #the previous room is being updated as well as the current room, the turn is being counted
-        #, the game is being checked if it's gameover and the new room is being displayed via message
         if next_room is None:
             self.msg = "You can't move in that direction."
         else:
@@ -89,36 +131,31 @@ class Game:
             if self.game_over():
                 return
 
-    #in this function this is the welcoming message to the game
     def set_welcome_message(self):
+        """Sets the welcome message for the game."""
         self.msg = "Welcome to the Quest Game! Escape the ROBLOX obby maze in the fewest turns possible."
 
-    #this tells what the current room looks like through a description
     def look(self):
+        """Sets the message to describe the current room."""
         self.msg = str(self.current)
 
-    #this shows all the moves that the player can do in the game
-    #ex - move certain direction, look, take, place a item, speak, use an item
-    #gamble, retreat, help, or quit, as well as the objective of the game
     def help(self):
+        """Sets the message to display available commands and the game objective."""
         self.msg = (
             "Commands: move <direction>, look, take, place <item>, items, speak, use <item>, "
             "gamble <odd/even>, retreat, help, quit.\nObjective: Escape by reaching Level 8."
         )
 
-    #this shows what the items the player are holder, it is also checking if the player is holding
-    #any items or if they aren't, they are told they aren't holding any items
     def items(self):
+        """Sets the message to list the items in the player's inventory."""
         if self.items:
             item_list = ", ".join([item.get_name() for item in self.items])
             self.msg = f"You are holding: {item_list}."
         else:
             self.msg = "You are not holding any items."
 
-    #this lets the player pick up an item and place it in their
-    #inventory, first it checks if there is item to take
-    #then checks if the item is too heavy to pick up, otherwise they take item
     def take(self):
+        """Adds an item in the current room to the player's inventory."""
         if not self.current.has_item():
             self.msg = "There is nothing to take."
         else:
@@ -130,10 +167,8 @@ class Game:
                 self.current.remove_item()
                 self.msg = f"You are now holding the {item.get_name()}."
 
-    #this places the item from the player's inventory in the current room
-    #first it checks if they're holding aw item, then checks if there is
-    #an item already in the room, otherwise the player puts an item in the room
     def place(self, item_name):
+        """Places an item from the player's inventory into the current room."""
         item = self.search_items(item_name)
         if not item:
             self.msg = f"You are not holding a {item_name}."
@@ -144,26 +179,23 @@ class Game:
             self.current.set_item(item)
             self.msg = f"You carefully place the {item_name} in the room."
 
-    #this searches for the item in the player's inventory through name
     def search_items(self, name):
+        """Searches for an item in the player's inventory by name."""
         for item in self.items:
             if item.get_name() == name:
                 return item
         return None
 
-    #this let the player talk to the NPC, it first checks if there is not a NPC is in the room
-    #if there is aw npc, the player speaks to the NPC
     def speak(self):
+        """Allows the player to interact with an NPC if one is present in the room."""
         if not self.current.has_npc():
             self.msg = "There is no one here to speak to."
         else:
             npc = self.current.get_npc()
             self.msg = npc.speak()
 
-    #this lets the player move to room they were in before
-    #if there isn't a previous room or if the player is in the starting room, they're aren't
-    #able to retreat, otherwise they can retreat
     def retreat(self):
+        """Allows the player to return to the previous room."""
         if self.previous_room is None or self.current == self.level1:
             self.msg = "You cannot retreat from here."
         else:
@@ -171,13 +203,8 @@ class Game:
             self.previous_room = None
             self.msg = f"You retreated. {self.current.get_description()}"
 
-    #this let the player use an item, first checks if they're holding an item, then
-    #checks if the player is holding the useable items (speedy cola, powershield, and teleporter),
-    #if they have them, they're able to use them to benefit them in the game
-    #otherwise the player is let known that they can't use the item at moment
-    #the speedy cola lets the player move twice in one turn and the teleporter teleports the player to a random level
-    #,the power shield protects the player sfrom traps 
     def use(self, item_name):
+        """Uses a specific item from the player's inventory."""
         item = self.search_items(item_name)
         if not item:
             self.msg = f"You are not holding a {item_name}."
@@ -195,10 +222,8 @@ class Game:
         else:
             self.msg = f"The {item_name} cannot be used right now."
 
-    #this is a gambling action that lets the player roll a dice, if they win they move forward
-    #and if they lose they go the previous level, the player picks between odds and even from
-    #the range of a 1-6 dice
     def gamble(self, choice):
+        """Lets the player gamble to move forward or return to a previous room."""
         roll = random.randint(1, 6)
         if (roll % 2 == 0 and choice == "even") or (roll % 2 == 1 and choice == "odd"):
             self.msg = f"You gambled and won! You skip this level."
@@ -207,9 +232,8 @@ class Game:
             self.msg = f"You gambled and lost! Returning to the previous level."
             self.retreat()
 
-    #this checks if the game is over, if the player is in level 8 they win the game
-    #it also checks if they exceeded 15 turns which means they lost the game
     def game_over(self):
+        """Checks if the game is over by either reaching the exit or exceeding max turns."""
         if self.current == self.level8:
             self.msg = "Congratulations! You have reached the exit and won the game!"
             return True
@@ -218,14 +242,14 @@ class Game:
             return True
         return False
 
-    #this moves the player to the winning level automatically
     def auto_win(self):
+        """Automatically moves the player to the winning level."""
         self.msg = "Starting auto-win sequence..."
         self.current = self.level8
         self.msg += " Game won successfully!"
 
-    #this reads what player types and enters it into a command
     def parse_command(self):
+        """Reads player input and parses it into a command and an argument."""
         words = input("Enter>>> ").strip().split()
         if not words:
             return None, None
@@ -233,9 +257,8 @@ class Game:
         arg = words[1] if len(words) > 1 else None
         return command, arg
 
-    #this starts the game loop where the player is able to type and enter stuff to play
-    #this also shows all the valid commands
     def play(self):
+        """Starts the game loop where the player can enter commands."""
         print(self.get_message())
         while True:
             command, arg = self.parse_command()
@@ -268,7 +291,6 @@ class Game:
                 self.msg = "Unknown command. Type 'help' for a list of commands."
             print(self.get_message())
 
-#this starts the game
 if __name__ == '__main__':
     g = Game()
     g.play()
